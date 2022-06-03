d88bc994700223b2756cb015f747bcb3d79f5db5
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-06-03 09:07:11 +0900

    lesson 5: ESLint with npm Scripts

--- src/example.js
+++ src/example.js
@@ -1,10 +1,12 @@
+'use strict'
+
 const username = 'freddy'
-typeof username === 'string'
+typeof username === 'strng'
 
 if (!('serviceWorker' in navigator)) {
   // you have an old browser :-(
 }
 
 const greeting = 'hello'
-console.log(`${greeting} world!`);
+console.log('${greeting} world!')
 [1, 2, 3].forEach(x => console.log(x))
