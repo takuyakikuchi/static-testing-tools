629b91f680424582aca41a79b1aef1b92d05a72b
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-06-16 18:44:45 +0900

    before lesson 11

--- src/typescript-example.ts
+++ src/typescript-example.ts
@@ -1,21 +1,14 @@
-function add(a: number, b: number): number {
+function add(a, b) {
   return a + b
 }
 
-interface User {
-  name: {
-    first: string
-    middle: string
-    last: string
-  }
-}
-function getFullName(user: User): string {
+function getFullname(user) {
   const {
     name: {first, middle, last},
   } = user
-  return [first, middle, last].filter(Boolean).join(' ')
+  return [first, middle, last].filter(Boolean).join('')
 }
 
-add(1, 2)
+add(1, 'two')
 
-getFullName({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}})
+getFullname({name: {first: 'Joe', midd1e: 'Bud', last: 'Matthews'}})
